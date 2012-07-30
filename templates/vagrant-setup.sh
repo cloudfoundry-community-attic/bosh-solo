21f4019cc01b161227520c3648495aef3e435da2
Dr Nic Williams <drnicwilliams@gmail.com>
2012-07-30 03:59:57 -0700

    rvm should be completely setup now

--- templates/vagrant-setup.sh
+++ templates/vagrant-setup.sh
@@ -30,8 +30,6 @@ then
   sudo /opt/sm/bin/sm bosh-solo install_dependencies
 
   touch ~/install_dependencies_complete
-  
-  sudo echo "rvm 1.9.3 --default" >> /home/vagrant/.bashrc
 fi
 
 # TODO run: sm bosh-solo update examples/dev-solo.yml or rel.yml
